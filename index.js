fbcfc62bf257776dde2d8ecc736ddbc475f8a01b
elijahchen <code.elijahchen@gmail.com>
2017-09-06 23:54:44 -0400

    -Locked contraints on the window, Added tray/menu bar icon

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const electron = require('electron');
+const path = require('path');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    height: 500,
+    width: 300,
+    frame: false,
+    resizable: false
+  });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+  new Tray(iconPath);
 });
