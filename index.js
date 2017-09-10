e735935cf9e0f6e3a713ed646de5de76046b5898
Elijah <elijah@Sunnys-MacBook.lan>
2017-09-10 16:58:27 -0400

    -App main window is now properly tethered to the icon position/click event

--- index.js
+++ index.js
@@ -19,7 +19,21 @@ app.on('ready', () => {
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
   tray = new Tray(iconPath);
-  tray.on('click', () => {
-    mainWindow.show();
+  tray.on('click', (event, bounds) => {
+    const { x, y } = bounds;
+    const { height, width } = mainWindow.getBounds();
+
+    if(mainWindow.isVisible()){
+      mainWindow.hide();
+    } else {
+      const yPosition  = process.platform === 'darwin' ? y : y - height;
+      mainWindow.setBounds({
+        x: x - width/2,
+        y: yPosition,
+        height: height,
+        width: width
+      });
+      mainWindow.show();
+    }
   });
 });
